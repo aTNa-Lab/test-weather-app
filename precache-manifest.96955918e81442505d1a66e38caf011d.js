c494aef33cba9b2d7c0a6ef63d66f4b28ebe7a52
atna <atay.narynov@gmail.com>
2024-12-07 14:13:03 +0600

    Updates

---
+++ precache-manifest.96955918e81442505d1a66e38caf011d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd880651c80174cd40976c185694585",
+    "url": "/test-weather-app/index.html"
+  },
+  {
+    "revision": "7896c0d10eded204d6d8",
+    "url": "/test-weather-app/static/css/main.edd90992.chunk.css"
+  },
+  {
+    "revision": "53b9469f7aa827ff5ce9",
+    "url": "/test-weather-app/static/js/2.77f579b7.chunk.js"
+  },
+  {
+    "revision": "fa6965d26a9a6baf24f856735056b065",
+    "url": "/test-weather-app/static/js/2.77f579b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7896c0d10eded204d6d8",
+    "url": "/test-weather-app/static/js/main.4837d92f.chunk.js"
+  },
+  {
+    "revision": "7e2eadeef208cf513800",
+    "url": "/test-weather-app/static/js/runtime-main.d4bfd013.js"
+  },
+  {
+    "revision": "6eb21da79303a35df1750607558ebeae",
+    "url": "/test-weather-app/static/media/bg.6eb21da7.svg"
+  },
+  {
+    "revision": "47f1e6bdd7af84a534875c40ed81aac6",
+    "url": "/test-weather-app/static/media/cloudy.47f1e6bd.svg"
+  },
+  {
+    "revision": "2ae50b330e08793b35ee4a5e729df69e",
+    "url": "/test-weather-app/static/media/darkBg.2ae50b33.svg"
+  },
+  {
+    "revision": "5f81c3a6815e7834163e83875312feb8",
+    "url": "/test-weather-app/static/media/github.5f81c3a6.svg"
+  },
+  {
+    "revision": "0f8ede749e5dc61255d557c6ffaf9a46",
+    "url": "/test-weather-app/static/media/haze.0f8ede74.svg"
+  },
+  {
+    "revision": "17e40bd5c94c2cbedac31d7d7de613d1",
+    "url": "/test-weather-app/static/media/heavy-rain.17e40bd5.svg"
+  },
+  {
+    "revision": "931f8d0e10c490b2770cd939a3f68802",
+    "url": "/test-weather-app/static/media/high-icon.931f8d0e.svg"
+  },
+  {
+    "revision": "43a903ed0bfd53764b61806544b02860",
+    "url": "/test-weather-app/static/media/humidity-icon.43a903ed.svg"
+  },
+  {
+    "revision": "4cadc629aa2ffd7dabafb0df86b890fb",
+    "url": "/test-weather-app/static/media/low-icon.4cadc629.svg"
+  },
+  {
+    "revision": "c2e06f0212a9f4db46120dcf7ae2cc1e",
+    "url": "/test-weather-app/static/media/partly-cloudy.c2e06f02.svg"
+  },
+  {
+    "revision": "2284050a3cba90029fd95ab123b2b0b8",
+    "url": "/test-weather-app/static/media/pressure-icon.2284050a.svg"
+  },
+  {
+    "revision": "0138fd80e586127094a2058ae6cfd5d7",
+    "url": "/test-weather-app/static/media/rain.0138fd80.svg"
+  },
+  {
+    "revision": "4a7dd48bb6842dfd0f5d6f4d339ac651",
+    "url": "/test-weather-app/static/media/sleet.4a7dd48b.svg"
+  },
+  {
+    "revision": "f6477891c68a906fcc6091c69b8adc4d",
+    "url": "/test-weather-app/static/media/snow.f6477891.svg"
+  },
+  {
+    "revision": "3402daa5b40fff8cc27934f5b4fe9f6d",
+    "url": "/test-weather-app/static/media/sunny.3402daa5.svg"
+  },
+  {
+    "revision": "d0dcc540816c0878adc943b4fcb54ba9",
+    "url": "/test-weather-app/static/media/thunderstorm.d0dcc540.svg"
+  },
+  {
+    "revision": "7004d253811665b97b8f072088ae6de9",
+    "url": "/test-weather-app/static/media/wind-icon.7004d253.svg"
+  }
+]);
